48ee0a5f864aadef17ae679d179e13a9cf1a16b7
Bernadette Davis <bsd231@nyu.edu>
2019-08-23 13:02:45 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,22 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  "SELECT MAX(gpa) FROM students;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  puts @db.execute("SELECT tardies FROM students;")
+  puts @db.execute("SELECT COUNT(tardies) FROM students WHERE students.tardies > 0;")
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade == 9;"
 end
